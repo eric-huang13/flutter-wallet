cfacdd90ad689221270f8aff9a446f9edf0fa3db
Eric Huang <erichuang676@gmail.com>
2021-10-28 11:17:29 -0400

    Added query for querying pylons only apps

--- lib/pylons_app.dart
+++ lib/pylons_app.dart
@@ -1,14 +1,8 @@
-import 'dart:async';
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:pylons_wallet/components/pylons_app_theme.dart';
 import 'package:pylons_wallet/ipc/ipc_engine.dart';
 import 'package:pylons_wallet/pages/routing_page.dart';
-import 'package:pylons_wallet/stores/wallet_store.dart';
-import 'package:pylons_wallet/stores/wallet_store_imp.dart';
-import 'package:pylons_wallet/utils/base_env.dart';
-import 'package:transaction_signing_gateway/gateway/transaction_signing_gateway.dart';
 import 'package:transaction_signing_gateway/model/wallet_public_info.dart';
 
 
